06d7222a51bf8c904f7ce35bda045b6cec131f3b
amichalo421 <andre.michalowski@gmail.com>
2019-11-15 10:57:44 +0100

    Less_mixins_added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Header from "./components/Header.js";
 
+import WelcomePage from './components/WelcomePage';
 import CharacterList from './components/CharacterList';
-import SearchForm from './components/SearchForm'
+import SearchForm from './components/SearchForm';
 
 import { Route } from 'react-router-dom';
 
@@ -10,8 +11,9 @@ export default function App() {
   return (
     <main>
       <Header />
-      <Route exact path='/Search' component={SearchForm} />
-      <Route exact path='/' component={CharacterList} />
+        <Route exact path='/Search' component={SearchForm} />
+        <Route exact path='/Characters' component={CharacterList} />
+        <Route exact path='/' component={WelcomePage} />
     </main>
   );
 }
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -5,11 +5,21 @@ export default function WelcomePage() {
     <section className="welcome-page">
       <header>
         <h1>Welcome to the ultimate fan site!</h1>
-        <img
-          className="main-img"
-          src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-          alt="rick"
-        />
+
+        <container className="container1">
+          <container className="containerA">
+            <img
+              className="main-img"
+              src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+              alt="rick"
+            />
+          </container>
+        </container>
+          <container className="containerB">
+            <a href="/Search">Search</a>
+            <a href="/Characters">Just Characters</a>
+          </container>
+
       </header>
     </section>
   );
