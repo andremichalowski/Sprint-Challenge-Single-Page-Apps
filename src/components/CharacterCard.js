d540f4b2e60ab82eb42690aecc11a8da7c012665
amichalo421 <andre.michalowski@gmail.com>
2019-11-15 10:11:12 +0100

    App_added

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -7,10 +7,10 @@ export default function CharacterCard(props) {
     <StyledCard>
       <div key={props.id}>
 
-      <h1>{props.character.name}</h1>
-      <img src={props.character.image} alt='RickAndMortyCharacter'/> 
-      <h3>Species: {props.character.species}</h3>
-      <h3>Status: {props.character.status}</h3>
+        <h1>{props.character.name}</h1>
+        <img src={props.character.image} alt='RickAndMortyCharacter'/> 
+        <h3>Species: {props.character.species}</h3>
+        <h3>Status: {props.character.status}</h3>
       </div>
     </StyledCard>
   );
