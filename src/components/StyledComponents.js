422392995f8050a2da9472c624720906e5c28217
amichalo421 <andre.michalowski@gmail.com>
2019-11-15 10:09:42 +0100

    Styled_components_added

---
+++ src/components/StyledComponents.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const StyledCard = styled.div`
+    padding: 25px;
+    margin: 15px;
+    width: 450px;
+    color: white;
+    background-color: lightblue;
+    border: 2px solid black;
+    border-radius: 5px;
+    
+    h1 {
+        color: gray;
+    }
+
+    h2 {
+        color: lightgray;
+    }
+`;
+
+export default StyledCard;
